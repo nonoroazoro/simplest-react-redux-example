aa3239dc45b1fbcfdab67596d1cae56198d27d61
nonoroazoro <xiaochao.k@gmail.com>
2016-08-09 22:53:22 +0800

    add react-hot-loader and HMR

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 ﻿const gulp = require("gulp");
 const gutil = require("gulp-util");
-const taskListing = require("gulp-task-listing");
 const webpack = require("webpack");
+const gTaskListing = require("gulp-task-listing");
 const WebpackDevServer = require("webpack-dev-server");
 
-const devConfig = require("./webpack.config");
-
-gulp.task("h", taskListing.withFilters(null, "h|help|default"));
+gulp.task("h", gTaskListing.withFilters(null, "h|help|default"));
 
 gulp.task("build", () =>
 {
+    const devConfig = require("./webpack.config");
     webpack(devConfig, (err, stats) =>
     {
         if (err)
@@ -22,16 +21,29 @@ gulp.task("build", () =>
 
 gulp.task("dev", () =>
 {
+    const port = "8080";
+    const ip = "http://localhost";
+    const devConfig = require("./webpack.config");
+
+    // 1. Hot module replacement of webpack-dev-server.
+    devConfig.entry.index.unshift(`webpack-dev-server/client?${ip}:${port}`, "webpack/hot/dev-server");
+    devConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    // 2. Hot module replacement of react-hot-loader.
+    devConfig.module.loaders[0].loaders.unshift("react-hot");
+
+    // 3. Config webpack-dev-server.
     new WebpackDevServer(webpack(devConfig), {
+        hot: true, // For Hot module replacement.
         contentBase: "./public",
         publicPath: devConfig.output.publicPath
-    }).listen(8080, "localhost", (err) =>
+    }).listen(port, "localhost", (err) =>
     {
         if (err)
         {
             throw new gutil.PluginError("webpack-dev-server", err);
         }
-        gutil.log("[webpack-dev-server]", "http://localhost:8080/");
+        gutil.log("[webpack-dev-server]", `${ip}:${port}`);
     });
 });
 
