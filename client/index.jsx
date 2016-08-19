28562098ff7595b7670fb0b6332e03ba9978a905
nonoroazoro <xiaochao.k@gmail.com>
2016-08-19 23:14:44 +0800

    add redux-promise

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,19 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import promiseMiddleware from "redux-promise";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+/**
+ * Apply redux-promise as a middleware.
+ */
+const store = createStore(
+    reducers,
+    applyMiddleware(promiseMiddleware)
+);
 
 ReactDOM.render(
     <Provider store={store}>
