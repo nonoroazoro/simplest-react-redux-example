084bbd60b2c106f76d94900add3b73b7fd01c93f
nonoroazoro <xiaochao.k@gmail.com>
2016-08-19 23:16:40 +0800

    add async actions

--- client/actions/CounterActionCreators.js
+++ client/actions/CounterActionCreators.js
@@ -3,7 +3,11 @@
  */
 
 import * as types from "./ActionTypes";
+import AsyncAPI from "../utils/AsyncAPI";
 
+/**
+ * Sync Action.
+ */
 export function increase(p_num = 1)
 {
     return {
@@ -14,12 +18,59 @@ export function increase(p_num = 1)
     };
 }
 
+/**
+ * Sync Action.
+ */
 export function decrease(p_num = 1)
 {
     return {
         type: types.COUNTER_DECREASE,
         payload: {
             num: p_num
+        }
+    };
+}
+
+/**
+ * Async Action.
+ */
+export async function increaseAsync(p_num = 1)
+{
+    let num = p_num;
+    try
+    {
+        num = await AsyncAPI.doSomeAsyncJob(p_num);
+    }
+    catch (e)
+    {
+    }
+
+    return {
+        type: types.COUNTER_INCREASE_ASYNC,
+        payload: {
+            num: num
+        }
+    };
+}
+
+/**
+ * Async Action.
+ */
+export async function decreaseAsync(p_num = 1)
+{
+    let num = p_num;
+    try
+    {
+        num = await AsyncAPI.doSomeAsyncJob(p_num);
+    }
+    catch (e)
+    {
+    }
+
+    return {
+        type: types.COUNTER_DECREASE_ASYNC,
+        payload: {
+            num: num
         }
     };
 }
