404ba1dee0679768c1c095ff59cabf6d10d6a2b0
nonoroazoro <xiaochao.k@gmail.com>
2016-08-19 23:16:06 +0800

    add async actions

--- client/reducers/counter.js
+++ client/reducers/counter.js
@@ -9,9 +9,11 @@ export default function counter(p_state = { value: 0 }, p_action)
     switch (p_action.type)
     {
         case types.COUNTER_INCREASE:
+        case types.COUNTER_INCREASE_ASYNC:
             return { value: p_state.value + p_action.payload.num };
 
         case types.COUNTER_DECREASE:
+        case types.COUNTER_DECREASE_ASYNC:
             return { value: p_state.value - p_action.payload.num };
 
         default:
