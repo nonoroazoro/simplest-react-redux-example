bab9afab5b2d2229b2d558d0dd06ceea70344429
nonoroazoro <xiaochao.k@gmail.com>
2016-08-19 23:17:10 +0800

    update to stateless function & add async actions

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -2,46 +2,39 @@
  * Counter is a Presentational Component.
  */
 
-import React, { Component, PropTypes } from "react";
+import React, { PropTypes } from "react";
 
-class Counter extends Component
+const Counter = (p_props) =>
 {
-    static propTypes = {
-        value: PropTypes.number.isRequired,
-        actions: PropTypes.shape({
-            increase: PropTypes.func.isRequired,
-            decrease: PropTypes.func.isRequired
-        })
-    };
+    const { value, actions: { increase, decrease, increaseAsync, decreaseAsync } } = p_props;
+    return (
+        <p>
+            <span>{value}</span>
+            {" "}
+            <button onClick={() => increase()}>+</button>
+            {" "}
+            <button onClick={() => decrease()}>-</button>
+            {" "}
+            <button onClick={() => increase(2)}>+2</button>
+            {" "}
+            <button onClick={() => decrease(3)}>-3</button>
+            {" "}
+            <button onClick={() => increaseAsync()}>Increment Async</button>
+            {" "}
+            <button onClick={() => decreaseAsync()}>Decrement Async</button>
+        </p>
+    );
+};
 
-    /**
-     * Method is not automatically bound to its component instance when using ES6 class syntax.
-     * We can manually bind this method with arrow function.
-     */
-    incrementAsync = () =>
-    {
-        setTimeout(this.props.actions.increase, 1000);
-    };
-
-    render()
-    {
-        const { value, actions: { increase, decrease } } = this.props;
-        return (
-            <p>
-                <span>{value}</span>
-                {" "}
-                <button onClick={() => increase()}>+</button>
-                {" "}
-                <button onClick={() => decrease()}>-</button>
-                {" "}
-                <button onClick={() => increase(2)}>+2</button>
-                {" "}
-                <button onClick={() => decrease(3)}>-3</button>
-                {" "}
-                <button onClick={this.incrementAsync}>Increment Async</button>
-            </p>
-        );
-    }
-}
+Counter.propTypes =
+{
+    value: PropTypes.number.isRequired,
+    actions: PropTypes.shape({
+        increase: PropTypes.func.isRequired,
+        decrease: PropTypes.func.isRequired,
+        increaseAsync: PropTypes.func.isRequired,
+        decreaseAsync: PropTypes.func.isRequired
+    })
+};
 
 export default Counter;
