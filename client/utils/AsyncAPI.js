90236bf66eef623b5bf2c12344d08e0cc32de790
nonoroazoro <xiaochao.k@gmail.com>
2017-01-18 21:35:46 +0800

    remove redundant await

--- client/utils/AsyncAPI.js
+++ client/utils/AsyncAPI.js
@@ -5,9 +5,9 @@
 /**
  * Do some async job and return the result.
  */
-async function doSomeAsyncJob(p_num = 1)
+function doSomeAsyncJob(p_num = 1)
 {
-    return await new Promise((resolve, reject) =>
+    return new Promise((resolve, reject) =>
     {
         setTimeout(resolve, 1000);
     }).then(() =>
